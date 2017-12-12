928659d91cbdd19cdcdf3ca1e83357483e6bf161
mumudong <850886448@qq.com>
2017-12-12 17:44:04 +0800

    mumu

--- code/python/numpyOne/__init__.py
+++ code/python/numpyOne/__init__.py
@@ -3,11 +3,11 @@ import numpy as np
 
 x = np.array([1,2])
 print(x)
-print(x.shape) # 1维时，相当于元素的个数
+print(x.shape,type(x)) # 1维时，相当于元素的个数
 
 y = np.expand_dims(x,axis=0)
 print(y)
-print(y.shape,y.ndim) # 2维时，相当于矩阵的形状
+print(y.shape,y.ndim,type(y)) # 2维时，相当于矩阵的形状
 
 z = np.expand_dims(x,axis=1)
 print(z)
@@ -20,6 +20,6 @@ print(d.shape)
 print(d.sum(axis=0))  # (3,3) axis=0之后，第一个表示行的三被去除，只剩下列了
 print(np.expand_dims(d,axis=2).shape)
 print(d)
-print(np.expand_dims(d,axis=3)) # 到底是怎么增维的？
+print(np.expand_dims(d,axis=1)) # 到底是怎么增维的？
 
 
