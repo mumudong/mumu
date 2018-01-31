a508b7b4959949004116aee0b21755cf7764d939
mumudong <850886448@qq.com>
2018-01-31 17:01:55 +0800

    mumu

--- code/python/Test.py
+++ code/python/Test.py
@@ -1,6 +1,8 @@
 y = [1] + [0] * 9
 x = [1] * 10
-def a():
+def a(): # 返回 Tuple
     return [x for _ in range(10)],[y for _ in range(10)]
 for t in a():
-    print(len(t))
+    print(t)
+for (x,y) in zip(range(4),range(4)):
+    print(x,y)
