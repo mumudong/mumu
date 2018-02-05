779c2e5b408ed3bc4c144de9ebb8ce172d59e63b
mumudong <850886448@qq.com>
2018-02-05 23:48:11 +0800

    python

--- code/python/tensorflow_test/CNN_mnist.py
+++ code/python/tensorflow_test/CNN_mnist.py
@@ -15,14 +15,16 @@ def bias_variable(shape):
     return initial
 """
   x是输入，W是卷积的参数,如[5,5,1,32],前两个参数是卷积核的尺寸
-  第三个代表有多少个channel，因为只有灰度单色，所以是1，若是RGB,则应为3
-  第四个参数代表卷积核的数量，即卷积层会提取多少类特征
+      第三个代表有多少个channel，因为只有灰度单色，所以是1，若是RGB,则应为3
+      第四个参数代表卷积核的数量，即卷积层会提取多少类特征
   strides代表卷积模板移动的步长，都是1代表不遗漏的划过图片每个点
-  padding代表边界的处理方式，same代表给边界加上padding让卷积的输出和输入保持同样的尺寸
+  padding代表边界的处理方式，same代表给边界加上padding让卷积的中心扫过所有点，输出和输入保持同样的尺寸
+                             valid则让边重合边
 """
 def conv2d(x,W):
     return tf.nn.conv2d(x,W,strides=[1,1,1,1],padding='SAME')
 """
+value [batch, height, width, channels]
 ksize表示池化的大小，即[batch,in_height,in_wide, in_channels]，目前一般batch和channels不做池化，对应为1
 strides表示在特征图上移动,[batch,in_height,in_wide, in_channels]
 """
@@ -57,6 +59,7 @@ b_fc1 = bias_variable([1024])
 h_pool2_flat = tf.reshape(h_pool2,[-1,7*7*64])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat,W_fc1) + b_fc1)
 
+
 #Dropout减轻过拟合
 keep_prob = tf.placeholder(tf.float32)
 h_fc1_drop = tf.nn.dropout(h_fc1,keep_prob)
@@ -70,7 +73,7 @@ y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop,W_fc2) + b_fc2)
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv),
                                               reduction_indices=[1]))
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-correct_prediction = tf.equal(tf.argmax(y_conv,1),tf.arg_max(y_,1))
+correct_prediction = tf.equal(tf.argmax(y_conv,1),tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
 
 tf.global_variables_initializer().run()
