e40c7c24b6b1c4314df73a0d515b138300a75c37
mumudong <850886448@qq.com>
2018-06-28 09:36:08 +0800

    mumu

--- code/python/pythonZero/Fish.py
+++ code/python/pythonZero/Fish.py
@@ -1,19 +1,28 @@
 #--coding:utf-8--
 class Fish:
     hungry = True
+    print(hungry)
+    def __init__(self):
+        print('init')
     def eat(self,food):
         if food is not None:
             self.hungry = False
 class User:
-    def __init__(self,name):
-        self.name = name
+    age = None
+    def __init__(self):
+        self.name = 'name'
+        self.age = 11
+    def pt(self):
+        print(self.name,self.age)
 def impor():
     print('import test')
 f = Fish()
-Fish.eat(f,None)
-print(f.hungry)
-Fish.eat(f,"grass")
-f.eat("grass")
-print(f.hungry)
-u = User("张三")
-print(u.name)
+u = User()
+u.pt()
+# Fish.eat(f,None)
+# print(f.hungry)
+# Fish.eat(f,"grass")
+# f.eat("grass")
+# print(f.hungry)
+# u = User("张三")
+# print(u.name)
